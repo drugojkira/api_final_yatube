b1f9a477fa4bce019d475eb1b92c2e8e0c17c761
Victor Black <drugojkira@rambler.ru>
2024-07-02 16:03:35 +0500

    test_9

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,8 +1,8 @@
-from rest_framework import permissions 
- 
- 
+from rest_framework import permissions
+
+
 class IsAuthorOrReadOnly(permissions.BasePermission): 
- 
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,56 +3,56 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField 
 from rest_framework.validators import UniqueTogetherValidator 
  
-from posts.models import Comment, Follow, Group, Post 
- 
-User = get_user_model() 
- 
- 
-class CommentSerializer(serializers.ModelSerializer): 
-    author = serializers.SlugRelatedField( 
-        read_only=True, slug_field='username' 
-    ) 
- 
-    class Meta: 
-        fields = '__all__' 
-        model = Comment 
-        read_only_fields = ('post',) 
- 
- 
-class GroupSerializer(serializers.ModelSerializer): 
- 
-    class Meta: 
-        model = Group 
-        fields = '__all__' 
- 
- 
-class PostSerializer(serializers.ModelSerializer): 
-    author = SlugRelatedField(slug_field='username', read_only=True) 
- 
-    class Meta: 
-        fields = '__all__' 
-        model = Post 
- 
- 
-class FollowSerializer(serializers.ModelSerializer): 
-    user = SlugRelatedField( 
-        slug_field='username', 
-        read_only=True, 
-        default=serializers.CurrentUserDefault() 
-    ) 
-    following = SlugRelatedField( 
-        slug_field='username', 
-        queryset=User.objects.all() 
-    ) 
- 
-    class Meta: 
-        fields = '__all__' 
-        model = Follow 
-        validators = [ 
-            UniqueTogetherValidator( 
-                queryset=Follow.objects.all(), fields=('following', 'user')) 
-        ] 
- 
+from posts.models import Comment, Follow, Group, Post
+
+User = get_user_model()
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(), fields=('following', 'user'))
+        ]
+
     def validate_following(self, data):
         if data == self.context['request'].user:
             raise serializers.ValidationError(
